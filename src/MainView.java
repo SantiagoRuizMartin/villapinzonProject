fccbbe714477c8bde4c2f27e47386580b8ec45a0
Santiago Ruiz <ext-santiago.ruiz@here.com>
2018-10-30 17:34:35 -0500

    new files were created

--- src/MainView.java
+++ src/MainView.java
@@ -5,21 +5,26 @@ import java.awt.event.MouseEvent;
 public class MainView {
     private JPanel panel1;
     private JLabel mapImage;
-    private JButton button1;
+    private JTextArea textArea1;
+    private JPanel secondaryPanel;
     private JButton acceptButton;
+    JScrollPane jsp;
     private double x, y;
 
     public MainView() {
 
-        mapImage.addMouseListener(new MouseAdapter() {
+                mapImage.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
                 x = e.getX();
                 y = e.getY();
+//                Location myLocation = getlocation(x, y);
+
                 System.out.println("Position x: " + x + " , Position y: " + y);
                 if (x >= 327.0 && x <= 372.0 && y >= 451.0 && y <= 473.0) {
-                    JOptionPane.showMessageDialog(null, "We are in Soatama");
+                    textArea1.setText("This is one of the most beautiful places in Villapinzon, there are more than 700" +
+                            "people living in this wonderful place");
                 }
             }
         });
